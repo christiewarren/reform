a8400ac76cc2d300c5cb3d82c52308bd14099e5b
Christina Warren <cew@christinas-mbp.home>
2021-03-17 22:40:17 -0600

    menu functionality

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ const scroll = new LocomotiveScroll({
    smooth: true,
    multiplier: 1
 });
+
+const menuBtn = document.getElementById('menuBtnWrap'),
+      menu = document.getElementById('menuWrap');
+
+menuBtn.addEventListener('click', () => {
+   if(menu.style.opacity == "0"){
+      menu.style.opacity = "1";
+      menu.style.transform = "translateX(0%)";
+   }else{
+      menu.style.opacity = "0";
+      menu.style.transform = "translateX(100%)";
+   }
+})
