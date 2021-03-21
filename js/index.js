71b5ee20d6530ee74458c47635030042db0f5253
Christina Warren <cew@christinas-mbp.home>
2021-03-21 13:39:29 -0600

    comment locomotive, add AOS, sticky working, and fix column prob on gallery

--- js/index.js
+++ js/index.js
@@ -1,24 +1,31 @@
-const scroll = new LocomotiveScroll({
-   el: document.querySelector('[data-scroll-container]'),
-   smooth: true,
-   multiplier: 1
-});
+// const scroll = new LocomotiveScroll({
+//    el: document.querySelector('[data-scroll-container]'),
+//    smooth: true,
+//    multiplier: 1
+// });
 
+//init butter.js
+// var canvas = document.getElementById('butterCanvas');
+// var butter = new Butter();
+// butter.sort(canvas);
+
+//show and hide menu
 const menuBtn = document.getElementById('menuBtnWrap'),
       menu = document.getElementById('menuWrap');
 
 menuBtn.addEventListener('click', () => {
-   if(menu.style.opacity == "0"){
-      menu.style.opacity = "1";
+   if(menu.style.display == "none"){
+      menu.style.display = "grid";
       menu.style.transform = "translateX(0%)";
+      
    }else{
-      menu.style.opacity = "0";
       menu.style.transform = "translateX(100%)";
+      menu.style.display = "none";
    }
 })
 
 function hideMenu(){
-   menu.style.opacity = "0";
+   menu.style.display = "none";
 }
 
 document.addEventListener('DOMContentLoaded', hideMenu);
