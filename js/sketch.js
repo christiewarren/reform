38c03a9705e9c85b39d26015d2deae02cbfe4ccf
Christina Warren <cew@Christinas-MacBook-Pro.local>
2021-04-01 21:01:41 -0600

    add poster drafts to landing animation and concept imagery draft unresponsive

---
+++ js/sketch.js
@@ -0,0 +1,36 @@
+let scl = 40;
+
+function setup(){
+   createCanvas(600, 600);
+ }
+ 
+ function draw(){
+   background(247, 247, 247);
+   stroke(36, 31, 33);
+   noFill();
+   frameRate(60);
+   strokeWeight(1);
+   //curves require additional points to tell how to enter & exit the curve
+
+   //make a vertical line every scl pixels across entire canvas
+   for(let x = -50; x <= width+100; x += scl){
+     beginShape();
+       curveVertex(mouseX*(x/200), mouseY); //enter curve point
+       curveVertex(x, 0);             //top point
+       //curveVertex(x, height/2);    //midpoint
+       curveVertex(x, height);        //bottom point
+       curveVertex(mouseX, mouseY);   //exit curve point
+     endShape();
+   }
+   
+   //make a horizontal line every scl pixels across entire canvas
+   for(let y = -50; y <= height+100; y += scl){
+     beginShape();
+     curveVertex(mouseX, mouseY*(y/200));   //enter curve point
+     curveVertex(0, y);             //left point
+     //curveVertex(width/2, y);     //midpoint
+     curveVertex(width, y);         //right point
+     curveVertex(-mouseX, -mouseY); //exit curve point
+     endShape();
+   }
+ }
