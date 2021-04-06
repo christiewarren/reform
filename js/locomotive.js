cd05a9ce05c031e694609d20cd6bceb8545fa405
Christina Warren <cew@Christinas-MacBook-Pro.local>
2021-04-06 10:47:47 -0600

    touch multiplier x5

--- js/locomotive.js
+++ js/locomotive.js
@@ -2,7 +2,7 @@ const scroll = new LocomotiveScroll({
    el: document.querySelector('[data-scroll-container]'),
    smooth: true,
    multiplier: 1,
-   touchMultiplier: 20,
+   touchMultiplier: 5,
    tablet: {
       smooth: true
    },
