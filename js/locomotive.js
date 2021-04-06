0ec0db9222b900ee47742d7093c1e58f336466bc
Christina Warren <cew@Christinas-MacBook-Pro.local>
2021-04-06 10:37:10 -0600

    add touch multiplier

--- js/locomotive.js
+++ js/locomotive.js
@@ -2,6 +2,7 @@ const scroll = new LocomotiveScroll({
    el: document.querySelector('[data-scroll-container]'),
    smooth: true,
    multiplier: 1,
+   touchMultiplier: 2,
    tablet: {
       smooth: true
    },
